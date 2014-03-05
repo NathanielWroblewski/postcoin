70b5eed2f471490dc336700aaa517a2048a004b9
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2014-03-04 21:49:23 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Postcoin::Application.config.secret_key_base = '381e7ee21cdb31f9e85146dd16199b8f0a3885abbbfbaa3e4bcbce71d69db4ce62e6d5e0a62bd32f7550243b39510f3fceac3060c49eaf16486528d3d9a49971'
