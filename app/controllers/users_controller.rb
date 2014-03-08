04fda4cb9ec2a2e6fa6e7f1ca64ce62a7574c1a7
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2014-03-08 01:36:14 -0800

    Deposit funds

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,17 @@ class UsersController < ApplicationController
   skip_before_filter :authenticate_user!, only: :new
 
   def new
-
+    @balance = fetch_balance(current_user.addresses.last.to_s)
   end
 
   def show
     @user = current_user
+  end
+
+  private
+
+  def fetch_balance(address)
+    response = HTTParty.get("http://mainnet.helloblock.io/addresses/#{address}")
+    response['data']['address']['balance']
   end
 end
