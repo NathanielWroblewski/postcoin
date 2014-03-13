343db1965ff60378369d84d2376828d6da90e4b2
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2014-03-12 20:20:37 -0700

    Skip transaction if email is postcoin

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -3,6 +3,7 @@ class EmailProcessor < ActiveRecord::Base
   def self.process(email)
     if sender = User.find_by(email: email.from)
       email.to.each do |recipient|
+        next if recipient[:email].include?('cc@postco.in')
         receiver = User.find_or_create_by(email: recipient[:email])
         Email.create(
           to: recipient[:email],
